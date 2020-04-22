6df35f64989377c993870da7df937997feafa145
nutel65 <44318430+nutel65@users.noreply.github.com>
2020-04-22 21:17:15 +0200

    testing

--- client.py
+++ client.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 import socket
+from server import exchange
 # from engine import utils
 
 HOST = '127.0.0.1'
-PORT = 23494
+PORT = 23232
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print("Connected")
-    print("sending message...")
-    s.sendall(b'Hello, world')
+    msg = input("send message:\n> ")
+    s.sendall(bytes(msg, "utf-8"))
+    print("message sent, waiting for reply...")
     data = s.recv(1024)
-    print('Received back:', repr(data))
+    print("received", data)
     input("press to exit")
--- server.py
+++ server.py
@@ -4,34 +4,37 @@ import socket
 from engine import utils
 
 HOST = '127.0.0.1'
-PORT = 23494
+PORT = 23232
 active_connections = []
 
-def handle_client(sock):
-    if len(active_connections) < 2:
-        active_connections.append(sock)
-        utils.log("Connected", type="SERVER")
-    else:
-        utils.log("Refused connection. Maximum client amount reached", type="SERVER")
-    with sock:
-        while True:
-            data = sock.recv(1024)
-            if data == b"":
-                utils.log("breaking connection...", type="SERVER")
-                break
-            sock.sendall(data)
-    active_connections.remove(sock)
+
+def exchange(sock, msg):
+    # if len(active_connections) < 2:
+    #     active_connections.append(sock)
+    #     utils.log("Connected", type="SERVER")
+    # else:
+    #     utils.log("Refused connection. Maximum client amount reached", type="SERVER")
+    utils.log("retrieving data from socket ...")
+    data = sock.recv(1024)
+    utils.log(f"data received: {data}")
+    msg = input("input message:\n>")
+    sock.sendall(bytes(msg, "utf-8"))
+    # active_connections.remove(sock)
     
 
 def main():
+    print(socket.gethostname())
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-        utils.log(f"Opened socket: {HOST}:{PORT}", type="SERVER")
+        utils.log(f"Running socket: {HOST}:{PORT}", type="SERVER")
         while True:
-            client_sock, addr = s.accept()
-            utils.log(f"Connecting ... ({addr[0]})", type="SERVER")
-            handle_client(client_sock)
+            utils.log("Waiting for client...", type="SERVER")
+            client1, addr = s.accept()
+            utils.log(f"Connecting with client ({addr[0]})", type="SERVER")
+            while True:
+                exchange(client1, "xd")
+            # client1.close()
 
 if __name__ == "__main__":
     main()
